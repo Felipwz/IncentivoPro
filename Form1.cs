2ec6af70a913bac16221eae598ba7a68295543a0
Felipwz <luizfelipesiqueira83@gmail.com>
2025-03-15 17:23:01 -0300

    Commit inicial

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace IncentivoPro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Modelos/Connection/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace IncentivoPro.Modelos.Connection
+{
+
+
+    public class AppDbContext : DbContext {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Aluno> Alunos { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Aluno>().ToTable("tbl_alunos");
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+    }
+
+
+
+}
